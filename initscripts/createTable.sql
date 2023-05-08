08e45e75ad0a22518e057164f68b44c626d02103
Aspyro95 <damien@ghysweb.be>
2023-05-08 14:28:39 +0200

    Mise à jour du script de création de table initiale

--- initscripts/createTable.sql
+++ initscripts/createTable.sql
@@ -16,3 +16,65 @@ INSERT INTO level01_shoppinglist
     VALUES ('Carotte', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
 INSERT INTO level01_shoppinglist
     VALUES ('Champignon', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Céleri', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Concombre', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Épinard', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Maïs', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Oignon', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Poivron', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Pomme de terre', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Salade', 'Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Tomate', 'Fruits/Légumes', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Banane', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Cerise', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Melon', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Orange', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Pomme', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Poire', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Raisin', 'Fruits', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Oeuf', 'Épicerie', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Pain', 'Épicerie', 'Kilogramme', 0, 3, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Confiture', 'Épicerie', 'Pièce', 0, 5, 1, 20);
+INSERT INTO level01_shoppinglist
+    VALUES ('Miel', 'Épicerie', 'Pièce', 0, 5, 1, 20);
+INSERT INTO level01_shoppinglist
+    VALUES ('Beurre', 'Épicerie', 'Pièce', 0, 5, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Lait', 'Épicerie', 'Pièce', 0, 10, 1, 5);
+INSERT INTO level01_shoppinglist
+    VALUES ('Yaourt', 'Épicerie', 'Pièce', 0, 20, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Fromage', 'Épicerie', 'Pièce', 0, 5, 1, 10);
+INSERT INTO level01_shoppinglist
+    VALUES ('Boeuf', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Poulet', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Dinde', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Porc', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Viande hachée', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Poisson', 'Viandes', 'Kilogramme', 0, 3, 1, 30);
+INSERT INTO level01_shoppinglist
+    VALUES ('Eau', 'Boissons', 'Litre', 0, 20, 1, 5);
